a06bc3081f78450e61d51f8a34d9a3a28768374e
Miguel <miguegomez2706@gmail.com>
2023-05-15 14:40:40 -0300

    Ejercicio2 terminado

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+# 2-Crea una función llamada saludo que tome el nombre de una persona como
+# parámetro e imprima un saludo personalizado.
+
+def saludo(nombre):
+    print(f"Hola {nombre}, como fue tu dia")
